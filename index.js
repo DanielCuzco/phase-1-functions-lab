b0772b9e9636c6479b0b1de89cd47bc02976706b
Daniel <daniel.cuzco@gmail.com>
2022-06-28 10:22:42 -0400

    commit tree 5d78d9a69f98d1d891129a58396eccbbaf00ca63

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(42 - pickupLocation);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return 264 * distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet(feet1, feet2) {
+    return Math.abs(feet1-feet2) * 264
+}
+
